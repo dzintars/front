20b3a66886cc789e0614704f9abbe895de4f6850
dzintars <dzintars.klavins@gmail.com>
2020-07-23 23:31:05 +0300

    chore(husky): Test

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,8 +1,8 @@
-const tasks = arr => arr.join(' && ');
+const tasks = arr => arr.join(' && ')
 
 module.exports = {
   hooks: {
-    'pre-commit': tasks(['lint-staged']),
+    'pre-commit': tasks(['echo "Hello from shell!"', 'git add .', 'lint-staged']),
     'commit-msg': tasks(['commitlint -E HUSKY_GIT_PARAMS']),
   },
-};
+}
