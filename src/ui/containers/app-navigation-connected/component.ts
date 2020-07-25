9696a7c03ce86fab9def7427315f7ce0c1546f10
dzintars <dzintars.klavins@gmail.com>
2020-07-25 16:35:37 +0300

    chore(app-navigation): remove redundant code

--- src/ui/containers/app-navigation-connected/component.ts
+++ src/ui/containers/app-navigation-connected/component.ts
@@ -2,19 +2,14 @@ import { AppNavigationElement } from '../../components/app-navigation'
 // import { customElement, property } from 'lit-element'
 // import { connect } from '@captaincodeman/redux-connect-element'
 import { connect } from '../../../utils/connect'
-import { store, RootState, ModuleSelectors, getApplications, getModules } from '../../../store'
+import { store, RootState, ModuleSelectors, RoutingSelectors } from '../../../store'
 
 // @customElement('app-navigation-connected')
 export class AppNavigationConnectedElement extends connect(store, AppNavigationElement) {
-  // constructor() {
-  //   super()
-  //   store.dispatch(getApplications())
-  //   store.dispatch(getModules())
-  // }
-
   // Map state to props (Connect lib)
   mapState(state: RootState) {
     return {
+      pathname: RoutingSelectors.pathname(state),
       modules: ModuleSelectors.selectModulesByApplicationId(state, { applicationId: this.appid }),
     }
   }
