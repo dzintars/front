230d2b112d1310b0f530d676762b462ce4c3b11e
dzintars <dzintars.klavins@gmail.com>
2020-07-25 16:28:23 +0300

    chore(view-dispatch): extend from base view

--- src/ui/views/view-dispatch/template.ts
+++ src/ui/views/view-dispatch/template.ts
@@ -1,17 +1,12 @@
 import { html, TemplateResult } from 'lit-element'
 import { ViewDispatchElement } from './component'
-import '../../containers/app-taskbar'
-import '../../containers/app-navigation-connected'
+// import '../../containers/app-taskbar'
 import '../../components/app-navigation'
+import '../../containers/app-navigation-connected'
 
 export default function template(this: ViewDispatchElement): TemplateResult {
   return html`
     <app-taskbar noshadow></app-taskbar>
-    <p>
-      Hello from
-      <strong>view-dispatch</strong>
-      ${this.name}.
-    </p>
     <a href="/">Home</a>
     <app-navigation appid="54789c07-bb43-4db4-8b2d-1a8e1f8c67f1" noshadow></app-navigation>
   `
