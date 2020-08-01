ba9268cd0f9a6fab6d54b603de277c3e33d8b0ec
dzintars <dzintars.klavins@gmail.com>
2020-08-01 13:54:52 +0300

    feat(view-sales): separates ui from state fix #97

--- src/ui/views/view-sales/component.ts
+++ src/ui/views/view-sales/component.ts
@@ -1,30 +1,22 @@
-import { LitElement, customElement, property, TemplateResult, CSSResultArray } from 'lit-element'
-import { ViewBaseElement } from '../view-base'
-import { connect } from '../../../utils/connect'
-import { store, RootState, ModuleSelectors, getApplications, getModules, startApplication } from '../../../store'
+import { LitElement, property, TemplateResult, CSSResultArray } from 'lit-element'
 import template from './template'
 import style from './style'
 
-@customElement('view-sales')
-export class ViewSalesElement extends connect(store, ViewBaseElement) {
-  @property({ type: String }) appid: string = 'c178025e-a209-4c50-8c34-36d35f36494c'
-
-  connectedCallback(): void {
-    super.connectedCallback()
-    store.dispatch(startApplication(this.appid))
-    // store.dispatch(getApplications())
-    // store.dispatch(getModules())
+export class ViewSalesElement extends LitElement {
+  static get is() {
+    return 'view-sales'
   }
+  @property({ type: String }) appid: string = 'c178025e-a209-4c50-8c34-36d35f36494c'
 
   protected render(): TemplateResult {
     return template.call(this)
   }
 
-  static get styles(): CSSResultArray {
+  public static get styles(): CSSResultArray {
     return [style]
   }
 
-  createRenderRoot(): Element | ShadowRoot {
+  protected createRenderRoot(): Element | ShadowRoot {
     return this.hasAttribute('noshadow') ? this : super.createRenderRoot()
   }
 }
